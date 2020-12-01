6fcd2eef94623fd9a8686e3d20ff3473708f5338
asaf <asaf@flexi-core.com>
2020-12-01 16:01:44 +0200

    prepare for release

---
+++ src/main/java/com/flexicore/translation/model/Translation.java
@@ -0,0 +1,54 @@
+package com.flexicore.translation.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.flexicore.model.Baseclass;
+import com.flexicore.security.SecurityContext;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Translation extends Baseclass {
+
+
+
+	private String externalId;
+	private String languageCode;
+	@ManyToOne(targetEntity = Baseclass.class)
+	private Baseclass translated;
+
+	public Translation() {
+	}
+
+	public Translation(String name, SecurityContext securityContext) {
+		super(name, securityContext);
+	}
+
+	public String getExternalId() {
+		return externalId;
+	}
+
+	public <T extends Translation> T setExternalId(String externalId) {
+		this.externalId = externalId;
+		return (T) this;
+	}
+
+	public String getLanguageCode() {
+		return languageCode;
+	}
+
+	public <T extends Translation> T setLanguageCode(String languageCode) {
+		this.languageCode = languageCode;
+		return (T) this;
+	}
+
+	@ManyToOne(targetEntity = Baseclass.class)
+	public Baseclass getTranslated() {
+		return translated;
+	}
+
+	public <T extends Translation> T setTranslated(Baseclass translated) {
+		this.translated = translated;
+		return (T) this;
+	}
+}
